eaee0e193b6e34e481261afc198b0f8f1c94eb2f
Anirudh <msanirudh1998@gmail.com>
2024-07-06 22:50:05 +0530

    Sorted characters

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-void exitIfNull (void *ptr) {
-    if (ptr == NULL) {
-        exit(ERROR_NULL_POINTER);
-    }
-}
-
 int main () {
     FILE *fptr;
     size_t readPtr;
@@ -21,19 +15,46 @@ int main () {
         readPtr = fread(buffer, sizeof(char), DEFAULT_BUFFER_SIZE, fptr);
         for (int i = 0; i < readPtr; i++) {
             const char current = *(buffer + i);
-            countArr[current]++;
+            countArr[current].n++;
+            countArr[current].c = current;
         }
     } while(!(readPtr < DEFAULT_BUFFER_SIZE));
 
-    for (int i = 0; i < COUNT_ARRAY_SIZE; i++) {
-        printf("%c : %ld\n", i, countArr[i]);
-    }
-
     // Sort character occurrences
+    sort(countArr, COUNT_ARRAY_SIZE);
+
+    for (int i = 0; i < COUNT_ARRAY_SIZE; i++) {
+        printf("%c : %ld\n", countArr[i].c, countArr[i].n);
+    }
 
 
     fclose(fptr);
     free(buffer);
 
     return 0;
+}
+
+void exitIfNull (void *ptr) {
+    if (ptr == NULL) {
+        printf("NULL pointer encountered. Exiting");
+        exit(ERROR_NULL_POINTER);
+    }
+}
+
+void sort (COUNT_ARRAY_TYPE* arr, int length) {
+    exitIfNull(arr);
+
+    COUNT_ARRAY_TYPE curr, prev;
+
+    for (int i = 0; i < length; i++) {
+        for (int j = 1; j < length - i; j++) {
+            prev = arr[j - 1];
+            curr = arr[j];
+
+            if (curr.n < prev.n) {
+                arr[j - 1] = curr;
+                arr[j] = prev;
+            }
+        }
+    }
 }
--- main.h
+++ main.h
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Define directives
 #define FILENAME "file.txt"
 #define DEFAULT_BUFFER_SIZE 100
 #define COUNT_ARRAY_SIZE 255
 
-typedef long COUNT_ARRAY_TYPE;
-
 #define ERROR_NULL_POINTER 1
 #define ERROR_FILE_LENGTH_CALC 2
+
+// Custom data types
+struct key_occurrence {
+    char c; // character
+    long n; // number of occurrences
+};
+
+// Custom type names
+typedef struct key_occurrence key_occurrence;
+typedef key_occurrence COUNT_ARRAY_TYPE;
+
+void exitIfNull (void *ptr);
+void sort (COUNT_ARRAY_TYPE* arr, int length);
